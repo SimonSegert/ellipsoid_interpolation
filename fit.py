8ace05d7f45aab945b8253a66a6342f0fab7c9e1
Simon Segert <simonsegert@gmail.com>
2021-11-30 16:16:35 -0600

    change to termination condition, use max value of quadratic form

--- fit.py
+++ fit.py
@@ -1,12 +1,22 @@
 import time
 import numpy as np
 
+def woodbury_update(Ainv,u,v):
+    #compute (A+uv^t)^{-1} given A^{-1},u,v
 
-def MinVolEllipse(P, tolerance,max_iter=100,use_naive=False,delta0=0,verbose=False):
+    uu=np.dot(Ainv,u)
+    vv=np.dot(Ainv,v)
+    denom=1+np.sum(v*uu)
+    return Ainv-np.outer(uu,vv)/(denom)
+
+
+
+
+def MinVolEllipse(Pt, tolerance,max_iter=100,use_naive=False,delta0=0,verbose=False):
     #adapted from https://www.mathworks.com/matlabcentral/fileexchange/9542-minimum-volume-enclosing-ellipsoid,
 
     #original matlab code assumes that first array dimension is embedding dimension,
-    P=P.T
+    P=np.copy(Pt).T
     d, N = P.shape
     Q = np.zeros((d+1,N))
     Q[0:d,:] = P[0:d,0:N]
@@ -17,36 +27,21 @@ def MinVolEllipse(P, tolerance,max_iter=100,use_naive=False,delta0=0,verbose=Fal
     u = (1/N) * np.ones((N,1))
     errs=[]
     START=time.time()
-    while err > tolerance:
-        #note: this can still be optimized considerably,
-        #because u1=au0+be_i (where e_i is standard basis vector),
-        #so X1=aX0+b Q[:,i]Q[:,i]^t (rank 1 update),
-        #we can calculate X1^{-1} by rank 1 update to (aX0)^{-1},
-        #https://math.stackexchange.com/questions/17776/inverse-of-the-sum-of-matrices,
-        #the rank 1 part is (aX0)^{-1} bQ[:,i]Q[:,i]^t (aX0)^{-1}=ba^{-2}*X0^{-1}Q[:,i] \\otimes (itself),
-        #(note that X is a symmetric matrix),
-
-        #we also need g=Tr((aX0)^{-1}bQ_iQ_i^t)=bQ_i^tX_0^{-1}Q[i]/a,
-
-        #so we can update the value of X and X^{-1} without the new einsum,
-        #X_1^{-1}=a^{-1}X0^{-1}-ba^{-2}OP(X0^{-1}Q[i])/(1+g),
-
-        #set c=ba^{-2}/(1+g), then,
-        #we have M1=a^{-1}M0-c diag(QX0^{-1}Q[i] x (transpose)),
-        #in general, diag(vw^t)=v (pointwise prod) w,
-        #thus we only need to compute QX0^{-1}Q[i],
+    pct_in_ellipse=0 #training samples inside of ellipse, used for stopping condition
+    while True:
         if count==1 or use_naive:
             X=np.einsum('ji,i,ki->jk',Q,u.squeeze(),Q,optimize='optimal')+delta0*np.eye(len(Q))
             #equivalent to X = np.linalg.multi_dot((Q,np.diag(u.squeeze()), Q.T)),
             Xinv=np.linalg.inv(X)
             M=np.einsum('ij,ik,kj->j',Q,Xinv,Q,optimize='optimal')
             #equivalent to M = np.diag(np.linalg.multi_dot((Q.T,np.linalg.inv(X), Q))),
+
+            #this matrix is maintained so that the partial quadratic forms can be quickly computed
+            PuPt_inv=np.linalg.inv(P@np.diag(u.squeeze())@P.T)
+            #PuPt'=aPuPt+bP_jP_j^t
+            #PuPt_inv'=a^{-1}inv(PuPt+(b/a)P_jP_j^t)
+
         else:
-            #u=(1-step_size) old_u+step_size*e_j,
-            #rank 1 updates, much faster than naive method,
-            #(note that np.dot is much faster than np.solve,
-            #so updating Xinv is much faster than updating X and then solving the system of eqns),
-            #v=np.linalg.solve(X,Q[:,j]),
             v=np.dot(Xinv,Q[:,j])
             a=1-step_size
             b=step_size
@@ -55,7 +50,10 @@ def MinVolEllipse(P, tolerance,max_iter=100,use_naive=False,delta0=0,verbose=Fal
             w=np.dot(Q.T,v)
             M=M/a-c*w*w
             #X=a*X+b*np.outer(Q[:,j],Q[:,j]),
+            #we don't use the woodbury update function here, because the intermediate values are also used to update m
             Xinv=Xinv/a-c*np.outer(v,v)
+
+            PuPt_inv=woodbury_update(PuPt_inv,(b/a)*P[:,j],P[:,j])/a
 
 
         maximum, j = np.max(M),np.argmax(M)
@@ -70,14 +68,27 @@ def MinVolEllipse(P, tolerance,max_iter=100,use_naive=False,delta0=0,verbose=Fal
             break
         if verbose and count%500==0:
             print(f'iter {count}, err={np.mean(errs[-50:])}')
+
+        #M/(d+1) are the values of the quadratic form of the ellipse at each data points
+        #cf. section 4 in Moshtagh paper
+        if maximum/(d+1)<1+tolerance:
+            break
+        #if count%100==0:
+            #U1 = np.diag(u.squeeze())
+            #A1 = (1 / d) * np.linalg.inv(P @ U1 @ P.T - (P @ u) @ (P @ u).T)
+            #c1=P@u
+            #A1=(1/d)*woodbury_update(PuPt_inv,c1,-c1)
+            #pct_in_ellipse=np.mean(dist_ellipsoid(Pt,A1,c1)<1+tolerance)
+            #A1=(PUP^T)^{-1}+b*OP((PUP^t)^{-1}P@u)
         errs.append(err)
         count = count + 1;
     if verbose:
         print('time per iteration:')
         print((time.time()-START)/(count-1))
     U = np.diag(u.squeeze())
-    A = (1/d) * np.linalg.inv(P @ U @ P.T - (P @ u)@(P@u).T )
     c = P @ u
+    A = (1 / d) * woodbury_update(PuPt_inv, c, -c)
+    #A = (1/d) * np.linalg.inv(P @ U @ P.T - (P @ u)@(P@u).T )
     return A,c,np.array(errs)
 
 def dist_ellipsoid(X,P,c):
@@ -108,7 +119,7 @@ def get_cropped_ds(ds,ds_test,padx,pady,ds_name,discard_const=False):
     #constant dimensions (e.g. pixels that are always black) cause instability in algorithm
     #(bc then the points lie on proper subspace)
     if discard_const:
-        nonconst_dims=np.std(X_tr,axis=0)>10**-5
+        nonconst_dims=np.where(np.std(X_tr,axis=0)>10**-6)[0]
         X_tr=X_tr[:,nonconst_dims]
         X_test=X_test[:,nonconst_dims]
     return X_tr,X_test
