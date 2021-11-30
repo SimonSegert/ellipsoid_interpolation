7d558be805085403a7aa0448ccd884ad1e0c5f28
Simon Segert <simonsegert@gmail.com>
2021-11-30 16:17:15 -0600

    minor changes to gaussian plotting

--- analyze_gaussian_results.py
+++ analyze_gaussian_results.py
@@ -3,10 +3,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data_dir='results/gaussian'
+data_dir='results/gaussian2'
 with open(data_dir+'/values.pkl','rb') as f:
     r,h=pkl.load(f)
 
+r=r[r['N']>1.1*r['d']]
 r=r.groupby(['d','N']).mean().reset_index()
 
 #for each dimension, the minimal dataset size needed for an interpolation prob of .5
@@ -14,7 +15,7 @@ critical_Ns=[]
 
 for d in np.unique(r['d']):
     ids=r['d']==d
-    plt.plot(r[ids]['N'],r[ids]['p'],label=d)
+    plt.plot(r[ids]['N'],r[ids]['p'],label=f'd={d}')
 
     #compute critical N
     #first make sure that the sizes are in sorted order
@@ -24,5 +25,14 @@ for d in np.unique(r['d']):
 
     critical_N=Ns[np.where(ps>.5)[0][0]]
     critical_Ns.append(critical_N)
+plt.axhline(.5,c='black',linestyle='--')
+plt.xlabel('N')
+plt.ylabel('interp prob')
 plt.legend()
+plt.savefig('results/gaussian/curves.pdf')
 
+plt.close()
+plt.scatter(np.unique(r['d'])[2:],critical_Ns[2:])
+plt.xlabel('d')
+plt.ylabel('critical N')
+plt.savefig('results/gaussian/criticalN.pdf')
